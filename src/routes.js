42b96cb82c949c1639a20684e33bb445e8dcfe14
gabriel.rosch <gabriel.rosch@veolia.com>
2023-02-25 08:15:01 -0300

    aula

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const routes = Router();
 const clientController = require('./modules/Cliente/ClienteControler')
 const livroController = require('./modules/Livro/LivroControler')
+const aluguelController = require('./modules/Aluguel/AluguelControler')
 
 
 routes.get('/',(req,res)=>{
@@ -14,5 +15,10 @@ routes.post('/cliente',clientController.create)
 routes.get('/livro',livroController.get)
 routes.post('/livro',livroController.create)
 routes.post('/livro/:id',livroController.putIsAlterado)
+
+
+routes.get('/aluguel',aluguelController.get)
+routes.post('/aluguel',aluguelController.create)
+routes.put('/aluguel',aluguelController.put)
 
 module.exports = {routes}
